69a3bd4f3ae75d81eca9ad5f1828968bc672b2ae
Luca <klatu1494@gmail.com>
2019-02-01 15:23:05 -0300

    Singleton pattern is not applied anymore in the Character class.

--- index.ts
+++ index.ts
@@ -3,16 +3,16 @@ addEventListener("load", () => {
     let drops: Array<Drop> = [];
 
     //player's character creation
-    let character: Character = Character.getInstance();
+    let player: Character = new Character();
 
     //movement modes select creation
     let movementModesSelect: HTMLSelectElement = document.createElement(
         "select"
     );
-    createMovementMode(character, "Standing", 1.1, movementModesSelect);
-    createMovementMode(character, "Sitting", 1.5, movementModesSelect);
-    createMovementMode(character, "Running", 0.7, movementModesSelect);
-    createMovementMode(character, "Walking", 1, movementModesSelect);
+    createMovementMode(player, "Standing", 1.1, movementModesSelect);
+    createMovementMode(player, "Sitting", 1.5, movementModesSelect);
+    createMovementMode(player, "Running", 0.7, movementModesSelect);
+    createMovementMode(player, "Walking", 1, movementModesSelect);
     document.body.appendChild(movementModesSelect);
 
     //drops table creation
